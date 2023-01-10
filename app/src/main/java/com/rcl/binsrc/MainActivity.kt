2dfdb8bf450aac081c39046338523334fdcc5333
RecodeLiner <Samir-ramazanov-1995@mail.ru>
2023-01-10 20:27:44 +0300

    Add Main Compose Screen

---
+++ app/src/main/java/com/rcl/binsrc/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.rcl.binsrc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+import com.rcl.binsrc.ui.theme.BinSRCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            BinSRCTheme {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                    ,
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    InputBlock()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputBlock(modifier: Modifier = Modifier) {
+    val padding = WindowInsets.systemBars.asPaddingValues()
+    val textState = remember { mutableStateOf("") }
+
+    Column{
+        TextField(
+            value = textState.value,
+            onValueChange = { textState.value = it },
+            label = { Text("Label") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding)
+                .border(1.dp, MaterialTheme.colorScheme.onSurface, RoundedCornerShape(4.dp))
+        )
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(LocalContext.current.getString(R.string.button_text))
+        }
+    }
+}
