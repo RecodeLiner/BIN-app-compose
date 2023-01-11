5d8f12f2888b5d8c32123874c9447080405d3dc5
RecodeLiner <Samir-ramazanov-1995@mail.ru>
2023-01-11 20:34:14 +0300

    split navigation

---
+++ app/src/main/java/com/rcl/binsrc/navigation/NavClass.kt
@@ -0,0 +1,22 @@
+package com.rcl.binsrc.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.rcl.binsrc.navigation.screens.HistoryScreen
+import com.rcl.binsrc.navigation.screens.MainScreen
+
+class NavClass{
+    @Composable
+    fun Navigation(navController: NavHostController) {
+        NavHost(navController = navController, startDestination = Routes.MainScreen.routes) {
+            composable(Routes.MainScreen.routes) {
+                MainScreen().Screen()
+            }
+            composable(Routes.HistoryScreen.routes) {
+                HistoryScreen().Screen()
+            }
+        }
+    }
+}
