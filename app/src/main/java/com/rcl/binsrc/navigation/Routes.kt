7c772be841761af34e0e83705f0d697d18e4c37e
RecodeLiner <Samir-ramazanov-1995@mail.ru>
2023-01-11 20:34:22 +0300

    update routes

--- app/src/main/java/com/rcl/binsrc/navigation/Routes.kt
+++ app/src/main/java/com/rcl/binsrc/navigation/Routes.kt
@@ -1,6 +1,6 @@
 package com.rcl.binsrc.navigation
 
 sealed class Routes(val routes: String) {
-    object MainList : Routes("MainList")
-    object AddUI : Routes("AddUI")
+    object MainScreen : Routes("MainScreen")
+    object HistoryScreen : Routes("HistoryScreen")
 }
