07e371e55f6397d67230771160a2da3d9086675f
RecodeLiner <Samir-ramazanov-1995@mail.ru>
2023-01-16 20:48:21 +0300

    try to fix null return on room

--- app/src/main/java/com/rcl/binsrc/room/Bin.kt
+++ app/src/main/java/com/rcl/binsrc/room/Bin.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "bin_object")
+@Entity(tableName = "bin_table")
 data class Bin(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "bin") val bin: String,
--- app/src/main/java/com/rcl/binsrc/room/BinDao.kt
+++ app/src/main/java/com/rcl/binsrc/room/BinDao.kt
@@ -7,12 +7,12 @@ import androidx.room.Query
 
 @Dao
 interface BinDao {
-    @Query("SELECT * FROM bin_object")
+    @Query("SELECT * FROM bin_table")
     fun getAll(): LiveData<List<Bin>>
 
     @Insert
     suspend fun insert(Bin: Bin)
 
-    @Query("DELETE FROM bin_object")
+    @Query("DELETE FROM bin_table")
     suspend fun deleteAll()
 }
